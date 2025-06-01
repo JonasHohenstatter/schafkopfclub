953d1a5bb0c17f285ab1bea6b0adc4e5ce815d24
jonashohenstatter <jonashohenstatter@web.de>
2025-06-01 22:50:45 +0200

    Deployment

--- app.py
+++ app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
+@app.route("/events")
+def events():
+	return render_template("events.html")
+
+@app.route("/news")
+def news():
+	return render_template("news.html")
+
+@app.route("/ranking")
+def ranking():
+	return render_template("ranking.html")
+
+@app.route("/participate")
+def participate():
+	return render_template("participate.html")
+
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
